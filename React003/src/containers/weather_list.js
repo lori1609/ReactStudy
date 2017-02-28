8536fecbfd63bf5b0d9578b8507abee014ea166f
lori1609 <zhukuilei@gmail.com>
2017-02-28 23:28:11 +0800

    Add Google Map
    Still have issue on map displaying, will come back to fix it.

--- React003/src/containers/weather_list.js
+++ React003/src/containers/weather_list.js
@@ -2,6 +2,7 @@ import React,{Component} from 'react';
 import { connect } from 'react-redux';
 
 import Chart from '../components/chart';
+import GoogleMap from '../components/google_map';
 
 class WeatherList extends Component {
 
@@ -11,9 +12,15 @@ class WeatherList extends Component {
       const pressures = cityData.list.map(weather => weather.main.pressure);
       const humidities = cityData.list.map(weather => weather.main.humidity);
 
+      // const lon = cityData.city.coord.lon;
+      // const lat = cityData.city.coord.lat;
+      const { lon, lat } = cityData.city.coord;
+
       return (
         <tr key={name}>
-          <td>{name}</td>
+          <td>
+            <GoogleMap lon={lon} lat={lat} />
+          </td>
           <td>
             <Chart data={temps} color="orange" units="K" />
           </td>
