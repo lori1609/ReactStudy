5d4932cbba34b04acbfc52373dfd11d4185896b5
lori1609 <zhukuilei@gmail.com>
2017-03-02 10:33:15 +0800

    Add new component posts_view

---
+++ React004-1/src/components/posts_new.js
@@ -0,0 +1,12 @@
+import React,{Component} from 'react';
+
+export default class PostsNew extends Component {
+
+    render() {
+        return (
+            <div>
+                Create Form
+            </div>
+        );
+    }
+}
