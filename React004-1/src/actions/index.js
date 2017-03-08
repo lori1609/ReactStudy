8f851d325ca62d0c7d30aaefda70d4d660f50fd9
lori1609 <zhukuilei@gmail.com>
2017-03-08 18:59:19 +0800

    Before adding context in component

--- React004-1/src/actions/index.js
+++ React004-1/src/actions/index.js
@@ -20,7 +20,7 @@ export function fetchPosts() {
 }
 
 export function createPost(props){
-  const request = axios.get(`${ROOT_URL}/posts${API_KEY}`, props);
+  const request = axios.post(`${ROOT_URL}/posts${API_KEY}`, props);
   console.log("createPost action is called");
   return {
     type: CREATE_POST,
--- React004-1/src/components/posts_new.js
+++ React004-1/src/components/posts_new.js
@@ -3,6 +3,9 @@ import { reduxForm } from 'redux-form';
 
 import { createPost } from '../actions/index';
 
+import { Link } from 'react-router';
+
+
 class PostsNew extends Component {
 
     render() {
@@ -35,6 +38,7 @@ class PostsNew extends Component {
                 </div>
             </div>
             <button type="submit" className="btn btn-primary" >Submit</button>
+            <Link to="/" className="btn btn-danger" > Cancel </Link>
           </form>
         );
     }
