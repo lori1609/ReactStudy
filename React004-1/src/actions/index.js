72bda558f87e20b6f29cba7edd44df0f58434ac0
lori1609 <zhukuilei@gmail.com>
2017-03-08 18:53:10 +0800

    Take the version of redux-form to 4.1.3 to fix the version issue
    Take the version of redux-form to 4.1.3 to fix the version issue
    issue is
    form onSubmit={handleSubmit(this.props.createPost)}

--- React004-1/src/actions/index.js
+++ React004-1/src/actions/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import axios from 'axios';
 
 export const FETCH_POSTS = 'FETCH_POSTS';
+export const CREATE_POST = 'CREATE_POST';
 
 // export default () => {
 //   return <div> List of Blogs </div>;
@@ -11,9 +12,18 @@ const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
 const API_KEY = '?key=1234567890zaqwsxcderfv';
 
 export function fetchPosts() {
-  const request = axios.get(`${ROOT_URL}/post${API_KEY}`);
+  const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
   return {
     type: FETCH_POSTS,
     payload: request
   };
+}
+
+export function createPost(props){
+  const request = axios.get(`${ROOT_URL}/posts${API_KEY}`, props);
+  console.log("createPost action is called");
+  return {
+    type: CREATE_POST,
+    payload: request
+  }
 }
--- React004-1/src/components/posts_new.js
+++ React004-1/src/components/posts_new.js
@@ -1,12 +1,69 @@
 import React,{Component} from 'react';
+import { reduxForm } from 'redux-form';
 
-export default class PostsNew extends Component {
+import { createPost } from '../actions/index';
+
+class PostsNew extends Component {
 
     render() {
+        const { fields: { title, categories, content}, handleSubmit } = this.props;
+        // const handleSubmit = this.props.handleSubmit
+        // const title = this.props.fields.title;
+
         return (
-            <div>
-                Create Form
+          <form onSubmit={handleSubmit(this.props.createPost)}>
+            <h3> Create A New Post </h3>
+            <div className={`form-group ${title.touched && title.invalid ? 'has-danger' : ''}`} >
+              <label>Title</label>
+              <input type="text" className="form-control" {...title} />
+              <div className="test-help" >
+                {title.touched ? title.error : ''}
+              </div>
             </div>
+            <div className="form-group" >
+              <label>Categories</label>
+              <input type="text" className="form-control" {...categories} />
+                <div className="test-help" >
+                  {categories.touched ? categories.error : ''}
+                </div>
+            </div>
+            <div className="form-group" >
+              <label>Content</label>
+              <textarea className="form-control" {...content} />
+                <div className="test-help" >
+                  {content.touched ? content.error : ''}
+                </div>
+            </div>
+            <button type="submit" className="btn btn-primary" >Submit</button>
+          </form>
         );
     }
 }
+
+//connect: first argument is mapStateToProps, 2nd is mapDispatchToProps
+//reduxForm: 1st is form config, 2nd is mapStateToProps, 3rd is mapDispatchToProps
+
+
+//add validate to form
+function validate(values) {
+  const errors = {};
+
+  if(!values.title){
+    errors.title = 'Enter a title';
+  }
+  if(!values.categories){
+    errors.categories = 'Enter a categories';
+  }
+  if(!values.content){
+    errors.content = 'Enter a content';
+  }
+
+  return errors;
+}
+
+
+export default reduxForm({
+  form: 'PostsNewForm',
+  fields: ['title','categories','content'],
+  validate
+}, null, { createPost })(PostsNew);
